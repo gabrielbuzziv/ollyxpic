99f5f3faf520389d93cb087edee6df4942ccaf01
= <=>
2017-07-03 16:36:41 -0300

    base url alterado no http

--- frontend/src/libs/http.js
+++ frontend/src/libs/http.js
@@ -2,8 +2,10 @@ import axios from 'axios'
 
 window.axios = axios
 
+const baseURL = window.location.href.split('/')[2].includes('ollyxpic') ? 'http://api.ollyxpic.com' : 'http://localhost:8888'
+
 window.axios = axios.create({
-    baseURL: 'http://localhost:8888/api/',
+    baseURL: baseURL,
     timeout: 3000,
     headers: {
         'Accept': 'application/json',
