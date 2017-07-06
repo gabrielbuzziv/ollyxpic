b18936619fcef5438d894376e475d93ea1de8bc5
= <=>
2017-07-06 09:32:34 -0300

    atualizado analytics

--- frontend/src/libs/analytics.js
+++ frontend/src/libs/analytics.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
+import router from 'router'
 
 Vue.use(VueAnalytics, {
-    id: 'UA-102137418-1'
+    id: 'UA-102137418-1',
+    router
 })
