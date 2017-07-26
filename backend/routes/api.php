0b47bffdc720b2075a0c4860cbd36e64a6fa66c6
= <=>
2017-07-26 11:43:24 -0300

    Started the damage calc

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -44,3 +44,5 @@ Route::post('contact', 'PageController@sendContact');
 
 Route::get('mvp/{mvp}', 'MVPController@show');
 Route::post('mvp', 'MVPController@calculate');
+
+Route::get('database', 'DatabaseController@update');
