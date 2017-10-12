85f4db107e92e40c28668d205ce16d1624bc4403
Wheatlorz <wheatlord123@hotmail.com>
2017-10-12 17:54:09 +0200

    Test adding news page.

---
+++ frontend/src/app/modules/pages/news/home/views/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <page-load>
+	
+	
+        <panel title="News" class="margin-bottom-50">
+            Ollyxpic is up and running again!
+        </panel>
+
+        
+    </page-load>
+</template>
--- frontend/src/app/modules/pages/routes.js
+++ frontend/src/app/modules/pages/routes.js
@@ -1,7 +1,9 @@
 import { routes as home } from './home'
 import { routes as contact } from './contact'
+import { routes as news } from './news'
 
 export default [
     ...home,
     ...contact,
+	...news,
 ]
