7ff5ed40cb9c5479e663c1a05773aaa3f2568186
Wheatlorz <wheatlord123@hotmail.com>
2017-10-12 18:18:14 +0200

    Testing

--- frontend/src/app/modules/pages/news/news/views/Index.vue
+++ frontend/src/app/modules/pages/news/news/views/Index.vue
@@ -1,11 +1,21 @@
 <template>
     <page-load>
-	
-	
-        <panel title="News" class="margin-bottom-50">
-            Ollyxpic is up and running again!
+        <page-title>
+            <img :src="image_path('item', 201)" class="margin-right-10">
+            News
+            <span>All about tibia</span>
+        </page-title>
+
+        <panel>
+            <p class="margin-bottom-0">
+            This is a TEST
+            </p>
         </panel>
 
-        
+        <panel>
+        	<p class="margin-bottom-0"
+        	This is another TEST
+        	</p>
+        </panel>
     </page-load>
 </template>
