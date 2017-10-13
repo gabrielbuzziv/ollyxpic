6ea6259b76f684d2cb4e7dcc1dfa82f2cae187a5
Wheatlorz <wheatlord123@hotmail.com>
2017-10-13 14:37:47 +0200

    1

--- frontend/src/app/modules/pages/news/news/views/Index.vue
+++ frontend/src/app/modules/pages/news/news/views/Index.vue
@@ -2,6 +2,9 @@
     <page-load>
         <panel title="News" class="margin-bottom-50">
             News regarding updates to Tibia will be added here.
+			<br><br><br>
+			<br><br><br>
+			<br><br><br>
         </panel>
 
         <div class="row">
