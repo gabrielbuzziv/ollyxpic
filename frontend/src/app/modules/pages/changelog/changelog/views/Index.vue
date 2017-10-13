bce7a4e5d05875ecc0dabd6330ffeb88aeb90709
Wheatlorz <wheatlord123@hotmail.com>
2017-10-13 13:36:23 +0200

    Fixed error in changelog

--- frontend/src/app/modules/pages/changelog/changelog/views/Index.vue
+++ frontend/src/app/modules/pages/changelog/changelog/views/Index.vue
@@ -1,6 +1,6 @@
 <template>
     <page-load>
-        <panel title="News" class="margin-bottom-50">
+        <panel title="Changelog" class="margin-bottom-50">
             Added Damage Protection, this is still beeing tested,
 			Spellcaster is still beeing tested aswell.
         </panel>
