0495068d7afb333fbfd60aabeacf0d4237d23f06
Wheatlorz <wheatlord123@hotmail.com>
2017-10-15 15:35:13 +0200

    Testing partners

--- frontend/src/app/modules/pages/partners/partners/views/Index.vue
+++ frontend/src/app/modules/pages/partners/partners/views/Index.vue
@@ -1,18 +1,29 @@
 <template>
     <page-load>
 	
-        <panel title="Holy Mouse" class="margin-bottom-50">
-		<a target="_blank" href="https://www.twitch.tv/brosehd">
-		<img border="0" alt="Holy Mouse" src="/src/assets/images/twitch.png" width="50" height="50">
-		</a>
-        </panel>	
+	<div class="row">		
+		<div class="col-sm-6">
+			<panel title="Holy Mouse" class="margin-bottom-50">
+				<a target="_blank" href="https://www.twitch.tv/brosehd">
+					<img border="0" alt="Holy Mouse" src="/src/assets/images/twitch.png" width="50" height="50">
+				</a>
+			</panel>
+		</div>
 	
-        <panel title="Ix Maxwell" class="margin-bottom-50">
-		<a target="_blank" href="https://www.twitch.tv/loremester">
-		<img border="0" alt="Ix Maxwell" src="/src/assets/images/twitch.png" width="50" height="50">
-		</a>
-        </panel>
+		<div class="col-sm-6">
+			<panel title="Ix Maxwell" class="margin-bottom-50">
+				<a target="_blank" href="https://www.twitch.tv/loremester">
+					<img border="0" alt="Ix Maxwell" src="/src/assets/images/twitch.png" width="50" height="50">
+				</a>
+			</panel>
+		</div>
+	</div>
 
+        <panel title="More to come" class="margin-bottom-50">
+			We will add more partners in the future, if you would like to partner<br>
+			with us please use the contact form.<br>
+        </panel>
+	
         <div class="row">
 
         </div>
