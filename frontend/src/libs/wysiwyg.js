bcb1663318b4b9138714e62aac929f603f97629e
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2017-10-18 14:47:45 -0200

    updated summernote

--- frontend/src/libs/wysiwyg.js
+++ frontend/src/libs/wysiwyg.js
@@ -3,15 +3,24 @@ import VueSummernote from 'vue-summernote'
 
 require('bootstrap')
 require('summernote/dist/summernote.css')
+require('codemirror')
+require('codemirror/mode/htmlembedded/htmlembedded')
 
 Vue.use(VueSummernote, {
     dialogsFade: true,
     lang: 'en-US',
     toolbar: [
-        ['style', ['bold', 'italic', 'underline', 'clear']],
+        ['style', ['bold', 'italic', 'underline', 'strikethrough', 'clear']],
         ['fontsize', ['fontsize']],
         ['color', ['color']],
         ['para', ['ul', 'ol', 'paragraph']],
+        ['insert', ['link', 'picture', 'video']],
+        ['table', ['table']]
     ],
-    disableResizeEditor: true
+    codemirror: {
+        theme: 'monokai',
+        htmlMode: true,
+        lineNumbers: true,
+        mode: 'text/html'
+    }
 })
