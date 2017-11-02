146363c009d04c61c7c246c42f594ac962654e22
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2017-11-02 15:29:25 -0200

    Fixed tibiabosses page, added option do add new currencies in world lists

--- frontend/src/app/modules/admin/worlds/views/List.vue
+++ frontend/src/app/modules/admin/worlds/views/List.vue
@@ -19,6 +19,8 @@
                     <tr>
                         <th>Name</th>
                         <th>Type</th>
+                        <th>Last Update</th>
+                        <th></th>
                         <th></th>
                     </tr>
                 </thead>
@@ -27,8 +29,26 @@
                     <tr v-for="world in worlds">
                         <td>{{ world.name }}</td>
                         <td>{{ world.type }}</td>
+                        <td>
+                            <span v-if="world.currencies && world.currencies.length">
+                                {{ getDateForHuman(world.currencies[0].created_at) }}
+                            </span>
+                        </td>
+                        <td width="480">
+                            <img :src="image_path_by_name('item', 'tibia coins')" class="inline margin-right-10" />
+
+                            <form action="" class="form-inline inline" @submit.prevent="addCurrency(world)">
+                                <div class="form-group">
+                                    <input type="text" name="buy" class="form-control" placeholder="Buy" v-model="world.buy" @keypress.13="addCurrency(world)">
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" name="sell" class="form-control" placeholder="Sell" v-model="world.sell" @keypress.13="addCurrency(world)">
+                                </div>
+                            </form>
+                        </td>
                         <td class="text-right">
-                            <router-link :to="{ name: 'admin.worlds.edit', params: { id: world.id } }" class="btn btn-xs">
+                            <router-link :to="{ name: 'admin.worlds.edit', params: { id: world.id } }"
+                                         class="btn btn-xs">
                                 <i class="mdi mdi-pencil-circle"></i>
                             </router-link>
                         </td>
@@ -46,7 +66,8 @@
         data () {
             return {
                 loading: false,
-                worlds: []
+                worlds: [],
+                currencies: {}
             }
         },
 
@@ -54,9 +75,35 @@
             load () {
                 services.fetchWorlds()
                     .then(response => {
-                        this.worlds = response.data
+                        this.worlds = response.data.map(world => {
+                            return {
+                                ...world,
+                                buy: '',
+                                sell: ''
+                            }
+                        })
                     })
             },
+
+            getDateForHuman (date) {
+                return moment.tz(date, "YYYY-MM-DD HH:mm:ss", 'America/New_York').fromNow()
+            },
+
+            addCurrency (world) {
+                if (world.buy == null || world.buy == '' || world.sell == null || world.sell == '') {
+                    this.$message.error('Fill Buy and Sell currency.')
+
+                    return false
+                }
+
+                const currencies = { buy: world.buy, sell: world.sell }
+
+                services.addCurrency(world.id, currencies)
+                    .then(response => {
+                        this.$message.success('Currency added with success.')
+                        this.load()
+                    })
+            }
 
 //            remove (post) {
 //                this.$confirm(`If you remove the post "${post.title}", you will not be able to recovery.`, 'Are you sure about this?', {
--- frontend/src/app/modules/pages/tibiabosses/tibiabosses/views/Index.vue
+++ frontend/src/app/modules/pages/tibiabosses/tibiabosses/views/Index.vue
@@ -1,26 +1,32 @@
 <template>
     <page-load>
         <page-title>
-            <img :src="image_path_by_name('item', 'bunnyslippers')" class="margin-right-10">
+            <img :src="image_path_by_name('creature', 'Ferumbras')" class="margin-right-10">
             Tibiabosses
-            <span>Supported Fansite</span>
+            <span class="margin-left-80">Supported Fansite</span>
         </page-title>
 
         <div class="row">
 
-	<div class="col-md-12">
-		<panel>
-			<center>We recently started out partnership with <a href="https://www.tibiabosses.com" target="_blank">Tibiabosses.com</a>, Here is some information about them.</center>
-		</panel>
-	</div>
-
             <div class="col-md-12">
-                <panel title="Information" class="margin-bottom-50">
-			Do you usualy just go check bosses randomly? no more, go check out <a href="https://www.tibiabosses.com" target="_blank">Tibiabosses.com</a> Here you will find detailed 				information about boss locations, When it was last killed and estimated time untill next respawn.<br><br>
+                <panel>
+                    <p>
+                        Do you usualy just go check bosses randomly? no more, go check out <a href="https://www.tibiabosses.com" target="_blank">tibiabosses.com</a>.
+                        Here you will find detailed information about boss locations, When it was last killed and estimated time untill next respawn.
+                    </p>
 
-			But thats not all, They also host contests, Picture of the week, And have a great rares market where you can find those items you always wanted.<br><br>
+                    <p>
+                        But thats not all, They also host contests, Picture of the week, And have a great rares market where you can find those items you always wanted.
+                    </p>
 
-			And do not forget to check out their "usefull stuff" menu you where you can find alot of good information.
+                    <p>
+                        And do not forget to check out their "usefull stuff" menu you where you can find alot of good information.
+                    </p>
+
+                    <a href="https://www.tibiabosses.com" target="_blank" class="btn btn-default margin-top-20">
+                        Go to Tibia Bosses
+                        <i class="mdi mdi-arrow-right margin-left-5"></i>
+                    </a>
                 </panel>
             </div>
         </div>
