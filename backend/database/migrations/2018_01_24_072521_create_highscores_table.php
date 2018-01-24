1d52f54c4ed4f9f63b9f4ae9f4d3c5f3138145e9
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2018-01-24 10:31:27 -0200

    Highscores, created migration, model and controller.

---
+++ backend/database/migrations/2018_01_24_072521_create_highscores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHighscoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('highscores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rank');
+            $table->string('name');
+            $table->string('vocation_id');
+            $table->integer('experience');
+            $table->integer('level');
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
