4f688e968a06daabb1bdf99568e1361e24d9afaf
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2018-01-31 17:16:30 -0200

    updated home layout, news layout, and added slugs to news

---
+++ backend/database/migrations/2018_01_31_124330_add_fields_in_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldsInPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('link')->nullable();
+            $table->string('comments')->nullable();
+            $table->boolean('hotnews')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('link');
+            $table->dropColumn('comments');
+            $table->dropColumn('hotnews');
+        });
+    }
+}
