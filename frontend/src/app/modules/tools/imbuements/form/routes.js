79d5b3d32928817cd1cd18fd3035990de09e27b9
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2018-02-02 11:57:07 -0200

    Started tweaking the imbuement tool

--- frontend/src/app/modules/tools/imbuements/form/routes.js
+++ frontend/src/app/modules/tools/imbuements/form/routes.js
@@ -1,9 +1,15 @@
 import Form from './views/Form'
+import Index from './views/Index'
 
 export default [
     {
         path: '/tools/imbuements',
         name: 'tools.imbuements',
         component: Form,
+    },
+    {
+        path: '/tools/imbuements/test',
+        name: 'tools.imbuements.test',
+        component: Index,
     }
 ]
---
+++ frontend/src/app/modules/tools/imbuements/form/views/Index.vue
@@ -0,0 +1,31 @@
+<template>
+    <page-load id="imbuements">
+        <pre>
+            Listar todos os imbuements registrados.
+            Para cada tier do imbuement deverá ter a opção de selecionar gold token.
+
+            A listagem de valores deverá mostrar o valor individual de cada imbuement
+            e no final exibir o valor total, assim deixando mais claro o quanto que
+            custa cada imbuement, e quais são os valores somados.
+        </pre>
+    </page-load>
+</template>
+
+<script>
+    import services from '../services'
+
+    export default {
+        data () {
+            return {
+                imbuements: []
+            }
+        },
+
+        mounted () {
+            services.fecthImbuements()
+                .then(response => {
+                    this.imbuements = { ...response.data, basic: 0, intricate: 0, powerful: 0 }
+                })
+        }
+    }
+</script>
