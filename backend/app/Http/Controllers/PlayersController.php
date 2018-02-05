86bab6148323f0e80dc69cd7fed4cb361cbfe819
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2018-02-05 10:06:13 -0200

    Player experience now show 30 last days instead of last month

--- backend/app/Http/Controllers/PlayersController.php
+++ backend/app/Http/Controllers/PlayersController.php
@@ -31,7 +31,7 @@ class PlayersController extends ApiController
             ->experience()
             ->where('name', $name)
             ->where('active', 1)
-            ->where('updated_at', '>=', Carbon::today()->subMonth())
+            ->where('updated_at', '>=', Carbon::today()->subDays(30))
             ->where('updated_at', '<=', Carbon::today())
             ->orderBy('updated_at', 'asc')
             ->get();
