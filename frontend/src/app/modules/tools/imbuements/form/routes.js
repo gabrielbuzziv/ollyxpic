b1342b6da0ba104111754104a6ce986adad86145
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2018-02-20 14:31:16 -0300

    Changed imbuements new tool url

--- frontend/src/app/modules/tools/imbuements/form/routes.js
+++ frontend/src/app/modules/tools/imbuements/form/routes.js
@@ -5,11 +5,6 @@ export default [
     {
         path: '/tools/imbuements',
         name: 'tools.imbuements',
-        component: Form,
-    },
-    {
-        path: '/tools/imbuements/test',
-        name: 'tools.imbuements.test',
         component: Index,
     }
 ]
